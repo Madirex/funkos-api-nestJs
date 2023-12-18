e70731963acb0d02de0480abe0955cf66ce1d8aa
Madirex <madirexland@gmail.com>
2023-12-18 19:54:02 +0100

    removed not used code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { ConfigModule } from '@nestjs/config'
 import { FunkosModule } from './funkos/funkos.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(), FunkosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FunkosModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/funkos/mappers/funko.mapper.ts
+++ src/funkos/mappers/funko.mapper.ts
@@ -5,16 +5,6 @@ import { v4 as uuidv4 } from 'uuid'
 import { UpdateFunkoDto } from '../dto/update-funko.dto'
 @Injectable()
 export class FunkoMapper {
-  // mapToDTO(funko: Funko): CreateFunkoDto {
-  //   const dto = new CreateFunkoDto()
-  //   dto.name = funko.name
-  //   dto.price = funko.price
-  //   dto.quantity = funko.quantity
-  //   dto.image = funko.image
-  //   dto.category = funko.category //TODO: reemplazar por Category
-  //   return dto
-  // }
-
   mapCreateToEntity(dto: CreateFunkoDto): Funko {
     const funko = new Funko()
     funko.id = uuidv4()
