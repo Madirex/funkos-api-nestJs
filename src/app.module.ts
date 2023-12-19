4358ee7a39ce8fd136de93e1ebc8949138b84afc
Madirex <madirexland@gmail.com>
2023-12-20 00:30:53 +0100

    Added Postgres database connection and Docker Compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common'
 import { FunkosModule } from './funkos/funkos.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [FunkosModule],
+  imports: [
+    FunkosModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password123',
+      database: 'FUNKOS_DB',
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: true,
+    }),
+    CategoriesModule,
+  ],
   controllers: [],
   providers: [],
 })
