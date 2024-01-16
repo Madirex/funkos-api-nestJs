9d254102c41cfe6dcdf2dcc5bf7068217f39f38c
Madirex Land <madirexland@gmail.com>
2024-01-16 20:21:41 +0100

    SSL implemented

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,20 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { config } from 'dotenv'
+import { getSSLOptions } from './config/ssl.config'
 
 /*
  * Función principal de la aplicación
  */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const httpsOptions = getSSLOptions()
+  const app = await NestFactory.create(AppModule, { httpsOptions })
   app.setGlobalPrefix(process.env.API_VERSION || 'v1')
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.API_PORT || 3000)
 }
 
 config()
-bootstrap()
+bootstrap().then(() =>
+  console.log(`🟢 Servidor abierto en puerto: ${process.env.API_PORT || 3000}`),
+)
